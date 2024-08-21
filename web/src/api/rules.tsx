a4eb0ee923366c54d1df04f09ea04c2521422c07
Sushant <sushant.dhiman9812@gmail.com>
2024-08-21 23:13:11 +0530

    added add rule api

--- web/src/api/rules.tsx
+++ web/src/api/rules.tsx
@@ -1,9 +1,11 @@
+const baseUrl = "http://127.0.0.1:8080/"
+
 export interface rule {
     type: string;
     endpoint: string;
     http_method: string;
-    bucket_capacity: string;
-    token_add_rate: string;
+    bucket_capacity: number;
+    token_add_rate: number;
 }
 
 interface getAllRuleResponse {
@@ -13,7 +15,7 @@ interface getAllRuleResponse {
 
 
 export async function getAllRules(): Promise<rule[]> {
-    const url = "http://127.0.0.1:8080/rules/all";
+    const url = `${baseUrl}rules/all`;
 
     try {
         const response = await fetch(url, {
@@ -31,5 +33,28 @@ export async function getAllRules(): Promise<rule[]> {
     } catch (error) {
         console.error("Failed to fetch rules:", error);
         throw error;
+    }
+}
+
+
+export async function createNewRule(rule: rule) {
+    const url = `${baseUrl}rules/add`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(rule)
+        })
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(errorText);
+        }
+    } catch (error) {
+        console.error("Failed to add rule: ", error)
+        throw error
     }
 }
--- web/src/components/AddNewRule.tsx
+++ web/src/components/AddNewRule.tsx
@@ -1,10 +1,47 @@
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
 import BackArrow from '../assets/BackArrow.png'
+import { createNewRule, rule } from '../api/rules';
 
 interface Props {
     closeAddNewRule: () => void
 }
 
+
+
 const AddNewRule: React.FC<Props> = ({ closeAddNewRule }) => {
+    const [apiEndpoint, setApiEndpoint] = useState('');
+    const [strategy,] = useState('TOKEN BUCKET');
+    const [httpMethod, setHttpMethod] = useState('GET');
+    const [bucketCapacity, setBucketCapacity] = useState('');
+    const [tokenAddRate, setTokenAddRate] = useState('');
+
+
+    async function addRule() {
+        const newRule: rule = {
+            bucket_capacity: Number.parseInt(bucketCapacity),
+            endpoint: apiEndpoint,
+            http_method: httpMethod,
+            token_add_rate: Number.parseInt(tokenAddRate),
+            type: strategy
+        }
+
+        if (bucketCapacity === "" || apiEndpoint === "" || httpMethod === "" || tokenAddRate === "" || strategy === "") {
+            toast.error("Please ensure entered data is valid.", {
+                style: customToastStyle
+            })
+            return
+        }
+
+        try {
+            await createNewRule(newRule)
+            closeAddNewRule()
+        } catch (e) {
+            toast.error("Unable to add rule: " + e)
+        }
+    }
+
     return (
         <div className="px-8">
             <div className='flex items-center mb-12'>
@@ -19,7 +56,10 @@ const AddNewRule: React.FC<Props> = ({ closeAddNewRule }) => {
             <p className='mb-2'>API Endpoint</p>
             <input
                 className="bg-slate-200 pl-4 pr-4 py-2 rounded-md  focus:outline-none w-auto"
+                required
                 placeholder="Ex: - /api/v1/create"
+                value={apiEndpoint}
+                onChange={(e) => setApiEndpoint(e.target.value)}
             />
 
             <br></br>
@@ -32,7 +72,10 @@ const AddNewRule: React.FC<Props> = ({ closeAddNewRule }) => {
 
             <p className='mb-2 mt-6'>HTTP Method</p>
             <select
-                className="bg-slate-200 px-4 py-2 rounded-md focus:outline-none w-auto appearance-none">
+                className="bg-slate-200 px-4 py-2 rounded-md focus:outline-none w-auto appearance-none"
+                value={httpMethod}
+                onChange={(e) => setHttpMethod(e.target.value)}>
+
                 <option value="GET">GET</option>
                 <option value="POST">POST</option>
                 <option value="DELETE">DELETE</option>
@@ -45,20 +88,36 @@ const AddNewRule: React.FC<Props> = ({ closeAddNewRule }) => {
             <input
                 className="bg-slate-200 pl-4 pr-4 py-2 rounded-md  focus:outline-none w-auto"
                 placeholder="Ex: - 10000"
+                value={bucketCapacity}
+                onChange={(e) => setBucketCapacity(e.target.value)}
             />
 
-            <p className='mb-2 mt-6'>Token Add Rate</p>
+            <p className='mb-2 mt-6'>Token Add Rate (per second)</p>
             <input
                 className="bg-slate-200 pl-4 pr-4 py-2 rounded-md  focus:outline-none w-auto"
                 placeholder="Ex: - 100"
+                value={tokenAddRate}
+                onChange={(e) => setTokenAddRate(e.target.value)}
             />
 
-            <button className="bg-sidebar-bg text-slate-200 py-2 px-4 rounded-md flex items-center mt-8" onClick={() => { }}>
+            <button className="bg-sidebar-bg text-slate-200 py-2 px-4 rounded-md flex items-center mt-8" onClick={() => {
+                addRule()
+            }}>
                 Add Rule
             </button>
 
+            <Toaster position="bottom-right" reverseOrder={false} />
         </div>
     )
 }
+
+
+const customToastStyle = {
+    background: "#333",
+    color: "#fff",
+    padding: "10px 20px",
+    borderRadius: "8px",
+    fontSize: "14px",
+};
 
 export default AddNewRule
