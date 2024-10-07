3ffc71feae925277e6340a8da4a4cdba336c9206
Aditya <rawataditya193@gmail.com>
2024-10-08 01:30:03 +0530

    Just trying to add a footer for small devices

--- web/src/components/Dashboard.tsx
+++ web/src/components/Dashboard.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react"
 import ContentArea from "./ContentArea"
 import Sidebar from "./SideBar"
+import Footer from "./Footer"
+
 
 const Dashboard: React.FC = () => {
     const [selectedPage, setSelectedPage] = useState('API_CONFIGURATION')
 
     return (
-        <div className="flex flex-row bg-global-bg space-x-4 p-3">
-            <Sidebar onSelectPage={setSelectedPage} />
+        <div className="w-screen flex flex-col lg:flex-row bg-global-bg space-x-4 p-3">
+            <div className="hidden lg:block">
+            <Sidebar onSelectPage={setSelectedPage} />   
+            </div>
             <ContentArea selectedPage={selectedPage} />
+            <div className="block lg:hidden">
+            <Footer>
+            </Footer>
+            </div>   
         </div>
     )
 }
---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white">
+        Footer
+    </footer>
+  )
+}
+
+export default Footer
