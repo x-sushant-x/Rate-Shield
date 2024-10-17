c9a3e3bc26b8b36a815bc175bd4f14adcb9c217d
Manik072 <manikgujral47@gmail.com>
2024-10-17 22:05:29 +0530

    minor design fixes for frontend

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -11,47 +11,50 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ onSelectPage }) => {
     return (
-        <div className="w-64 h-screen bg-sidebar-bg text-gray-300 rounded-xl">
-            <div className="p-6">
-                <img src={logo} />
+        <div className="w-64 bg-sidebar-bg text-gray-300 rounded-xl flex flex-col justify-between">
+            <div>
+                <div className="p-6">
+                    <img src={logo} />
+                </div>
+                <ul>
+                    <div className='flex items-center ml-7 mt-4 cursor-pointer'
+                        onClick={() => onSelectPage('API_CONFIGURATION')}>
+                        <img src={apiIcon} />
+                        <li className="ml-4 hover:bg-gray-700 text-sm">API Configuration</li>
+                    </div>
+
+                    <div className='flex items-center ml-7 mt-4 cursor-pointer'
+                        onClick={() => onSelectPage('ABOUT')}>
+                        <img src={infoIcon} />
+                        <li className="ml-4 hover:bg-gray-700 text-sm">About</li>
+                    </div>
+
+                    <div className='flex items-center ml-7 mt-4 cursor-pointer'
+                        onClick={() => onSelectPage('TWITTER')}>
+                        <img src={twitterIcon} />
+                        <li className="ml-4 hover:bg-gray-700 text-sm">Follow on X</li>
+                    </div>
+
+                    <div className='flex items-center ml-7 mt-4 cursor-pointer'
+                        onClick={() => onSelectPage('LINKEDIN')}>
+                        <img src={linkedinIcon} />
+                        <li className="ml-4 hover:bg-gray-700 text-sm">Follow on LinkedIn</li>
+                    </div>
+
+                    <div className='flex items-center ml-7 mt-4 cursor-pointer'
+                        onClick={() => onSelectPage('GITHUB')}>
+                        <img src={githubIcon} />
+                        <li className="ml-4 hover:bg-gray-700 text-sm">Follow on GitHub</li>
+                    </div>
+                </ul>
             </div>
-            <ul>
-                <div className='flex items-center ml-7 mt-4 cursor-pointer'
-                    onClick={() => onSelectPage('API_CONFIGURATION')}>
-                    <img src={apiIcon} />
-                    <li className="ml-4 hover:bg-gray-700 text-sm">API Configuration</li>
-                </div>
 
-                <div className='flex items-center ml-7 mt-4 cursor-pointer'
-                    onClick={() => onSelectPage('ABOUT')}>
-                    <img src={infoIcon} />
-                    <li className="ml-4 hover:bg-gray-700 text-sm">About</li>
-                </div>
-
-                <div className='flex items-center ml-7 mt-4 cursor-pointer'
-                    onClick={() => onSelectPage('TWITTER')}>
-                    <img src={twitterIcon} />
-                    <li className="ml-4 hover:bg-gray-700 text-sm">Follow on X</li>
-                </div>
-
-                <div className='flex items-center ml-7 mt-4 cursor-pointer'
-                    onClick={() => onSelectPage('LINKEDIN')}>
-                    <img src={linkedinIcon} />
-                    <li className="ml-4 hover:bg-gray-700 text-sm">Follow on LinkedIn</li>
-                </div>
-
-                <div className='flex items-center ml-7 mt-4 cursor-pointer'
-                    onClick={() => onSelectPage('GITHUB')}>
-                    <img src={githubIcon} />
-                    <li className="ml-4 hover:bg-gray-700 text-sm">Follow on GitHub</li>
-                </div>
-            </ul>
-
-            <div className='text-sm ml-6 mt-[40rem]'>
-                Created for fun by <b>Sushant</b>
+            <div className='text-sm ml-6 mb-4'>
+                {/* Created for fun by <b>Sushant</b> */}
+                Still Under Development
             </div>
         </div>
     );
 };
 
-export default Sidebar
+export default Sidebar;
